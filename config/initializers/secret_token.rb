658b67ecc759b8c59effa05551db1bc2991b7e50
Rob Dawson <robert.dawson@ephox.com>
2011-03-06 21:52:56 +1000

    Initial Commit.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+DemoApp::Application.config.secret_token = '98ef4d2ffb105286725bd162c18de2279a76453c8809cfbf8f1ae9d0f42a4f88d933ca1eda74ba59f5b0a55dba568571767e152af304d858100980bb0d7ac348'
